c6524b2a75f8c3e5fae373fba61b84518ebbbbba
Steve Ford <sford@geeky-boy.com>
2024-11-21 17:30:12 -0500

    updates

--- cfg.h
+++ cfg.h
@@ -1,4 +1,4 @@
-/* err.h - simple error handler. */
+/* cfg.h - simple error handler. */
 /*
 # This code and its documentation is Copyright 2024-2024 Steven Ford, http://geeky-boy.com
 # and licensed "public domain" style under Creative Commons "CC0": http://creativecommons.org/publicdomain/zero/1.0/
