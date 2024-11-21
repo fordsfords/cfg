c6c2848bbd01fcb2f7a435b80df299e84ae69a50
Steve Ford <sford@geeky-boy.com>
2024-11-21 18:00:04 -0500

    updates

--- cfg_test.c
+++ cfg_test.c
@@ -1,4 +1,4 @@
-/* cfg_tst.c - self-test.
+/* cfg_test.c - self-test.
  *
  * This code and its documentation is Copyright 2019, 2019 Steven Ford,
  * http://geeky-boy.com and licensed "public domain" style under Creative
