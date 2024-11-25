e01d47143f06dbc835641ada9b811e0f6a66b904
Steve Ford <sford@geeky-boy.com>
2024-11-25 11:25:02 -0500

    updates

--- bld.sh
+++ bld.sh
@@ -14,6 +14,6 @@ echo "Building code"
 
 rm -f cfg_test
 
-gcc -Wall -pthread -g -o cfg_test -pthread cfg.c hmap.c err.c cfg_test.c; if [ $? -ne 0 ]; then exit 1; fi
+gcc -std=c99 -pedantic -Wall -Wextra -Werror -g -o cfg_test cfg.c hmap.c err.c cfg_test.c; if [ $? -ne 0 ]; then exit 1; fi
 
 echo "Build successful"
